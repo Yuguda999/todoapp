48982272356647309d145dcae8eb868c9308ce09
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-03-27 12:47:24 +0100

    used mysql database

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tododb',
-        'USER': 'postgres',
-        'PASSWORD': '#Yuguda26692669',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_database',
+        'USER': 'root',
+        'PASSWORD': 'your_password',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
